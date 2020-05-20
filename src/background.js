c2c293d95caff743026de30acfd9f5b833702f8b
Andreas Källberg <andreas@svalna.se>
2020-05-20 12:36:32 +0200

    Format code with prettier

--- src/background.js
+++ src/background.js
@@ -1,21 +1,32 @@
 var requestFilter = {
-	urls: [
-		"http://sh.st/*",
-		"https://sh.st/*",
-		"http://clkmein.com/*",
-		"http://viid.me/*",
-		"http://xiw34.com/*",
+  urls: [
+    "http://sh.st/*",
+    "https://sh.st/*",
+    "http://clkmein.com/*",
+    "http://viid.me/*",
+    "http://xiw34.com/*",
     "http://corneey.com/*",
     "http://gestyy.com/*",
     "http://cllkme.com/*",
     "http://festyy.com/*",
     "http://destyy.com/*",
     "http://ceesty.com/*",
-	]
+  ],
 };
 
-chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
-	var headers = details.requestHeaders;
-headers = headers.map( function(x) { if (x.name === 'User-Agent') {x.value = ''; return x} else {return x};});
-	return {requestHeaders: headers};
-}, requestFilter, ['requestHeaders','blocking']);
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  function (details) {
+    var headers = details.requestHeaders;
+    headers = headers.map(function (x) {
+      if (x.name === "User-Agent") {
+        x.value = "";
+        return x;
+      } else {
+        return x;
+      }
+    });
+    return { requestHeaders: headers };
+  },
+  requestFilter,
+  ["requestHeaders", "blocking"]
+);
